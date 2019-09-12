a37078ea8d1eecaa6f90ebae6950dda38fe65e4a
Luciferseva <36321675+Luciferseva@users.noreply.github.com>
2019-09-12 16:24:24 +0800

    Add files via upload

---
+++ encpy.py
@@ -0,0 +1,127 @@
+#作者:smallfoxdog 小狐狸狗狗
+#时间:2018年3月20日13:57:04
+#功能:文件的加密和解密
+
+import os
+
+#主函数
+def main():
+    getInput()
+
+#输入参数
+def getInput():
+
+    #获取操作的参数
+    while(True):
+
+        oper = input("请输入操作(e:加密 d:解密):")
+
+        if(oper=="e" or oper=="d"):
+            break
+        else:
+            print("输入有误，请重新输入!")
+
+    #获取文件密码
+    while(True):
+
+       password= input("请输入密码:")
+
+       if(len(password)==0):
+            print("密码不能为空!")
+       else:
+           break
+
+    #获取操作的文件路径
+    while(True):
+
+        path=input("请输入文件路径:")
+
+        try:
+            f_read  = open(path,"rb")
+        except:
+            print("文件没有找到，请检查路径是否存在！")
+        else:
+            break
+
+    #进行加密或解密操作
+    if(oper=="e"):
+        encrypt(path,password)
+    elif(oper=="d"):
+        decrypt(path, password)
+
+#加密
+def encrypt(path,password):
+
+    #因为刚学可能有库可以直接获取这些信息吧，不过自己写个算法获取这些信息也没什么难度
+    fileFullName = path.split(os.path.sep)#os.path.sep为操作系统的文件分隔符
+    print(fileFullName)
+    fileName = fileFullName[len(fileFullName)-1].split(",")[0]
+    print(fileName)
+    #fileSuffix = fileFullName[len(fileFullName)-1].split(",")[1]
+
+    # print("文件全名称:",fileFullName[len(fileFullName)-1])
+    # print("文件名称:",fileName)
+    # print("文件后缀:",fileSuffix)
+
+    fileParent = path[0:len(path)-len(fileFullName[len(fileFullName)-1])]
+    newFileName="加密_"+fileFullName[len(fileFullName)-1]
+    newFilePath=fileParent+newFileName
+
+    # print("文件父路径:",fileParent)
+    # print("新的文件名称:",newFileName)
+    # print("新的文件全路径:", newFilePath)
+
+    f_read  = open(path,"rb")
+    f_write = open(newFilePath,"wb")
+
+    count=0 #当前密码加密索引
+
+    #我们采用异或循环加密
+    for now in f_read:
+        for nowByte in now:
+            newByte=nowByte^ord(password[count%len(password)])
+            count+=1
+            f_write.write(bytes([newByte]))
+
+    f_read.close()
+    f_write.close()
+
+    print("汪~文件加密完毕^_^")
+
+#解密（因为我们采取的异或解密，所以其实和加密算法一样）
+def decrypt(path, password):
+    # 因为刚学可能有库可以直接获取这些信息吧，不过自己写个算法获取这些信息也没什么难度
+    fileFullName = path.split(os.path.sep)  # os.path.sep为操作系统的文件分隔符
+    fileName = fileFullName[len(fileFullName) - 1].split(".")[0]
+    #fileSuffix = fileFullName[len(fileFullName) - 1].split(".")[1]
+
+    # print("文件全名称:", fileFullName[len(fileFullName)-1])
+    # print("文件名称:", fileName)
+    # print("文件后缀:", fileSuffix)
+
+    fileParent = path[0:len(path) - len(fileFullName[len(fileFullName)-1])]
+    newFileName = "解密_" + fileFullName[len(fileFullName) - 1]
+    newFilePath = fileParent + newFileName
+
+    # print("文件父路径:", fileParent)
+    # print("新的文件名称:", newFileName)
+    # print("新的文件全路径:", newFilePath)
+
+    f_read = open(path, "rb")
+    f_write = open(newFilePath, "wb")
+
+    count = 0  # 当前密码加密索引
+
+    # 我们采用异或循环加密
+    for now in f_read:
+        for nowByte in now:
+            newByte = nowByte ^ ord(password[count % len(password)])
+            count += 1
+            f_write.write(bytes([newByte]))
+
+    f_read.close()
+    f_write.close()
+
+    print("汪~文件解密完毕^_^")
+
+main()
